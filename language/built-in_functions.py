c72328cb99acc3d7ab0ceb72bf4065e2689cc827
Chris Cheng <yifan.cheng.1998@gmail.com>
2024-03-27 22:23:51 +1100

    change partial output symbols from >> to >>:

--- language/built-in_functions.py
+++ language/built-in_functions.py
@@ -5,7 +5,7 @@ def all_trial():
     bool_list1 = []
     bool_list2 = [True, False]
     bool_list3 = [True, True]
-    print('bool_list1:', all(bool_list1)) # >> True # *
+    print('bool_list1:', all(bool_list1)) # >>: True # *
     print('bool_list2:', all(bool_list2))
     print('bool_list3:', all(bool_list3))
 
@@ -13,33 +13,33 @@ def any_trial():
     bool_list1 = []
     bool_list2 = [True, False]
     bool_list3 = [True, True]
-    print('bool_list1:', any(bool_list1)) # >> False # *
+    print('bool_list1:', any(bool_list1)) # >>: False # *
     print('bool_list2:', any(bool_list2))
     print('bool_list3:', any(bool_list3))
 
 def bin_trial():
     num1 = 10
     num2 = -10
-    print('num1:', bin(num1)) # >> 0b1010
-    print('num2:', bin(num2)) # >> -0b1010
+    print('num1:', bin(num1)) # >>: 0b1010
+    print('num2:', bin(num2)) # >>: -0b1010
     # related functions
     print("---- related functios -----")
-    print('format(10, "#b")', format(10, '#b')) # >> 0b1010 # *
-    print('format(10, "b")', format(10, 'b')) # >> 1010 # *
-    print("abc{10:#b}def:", f'abc{10:#b}def') # >> abc0b1010def # *
-    print("abc{10:b}def:", f'abc{10:b}def') # >> abc1010def # *
+    print('format(10, "#b")', format(10, '#b')) # >>: 0b1010 # *
+    print('format(10, "b")', format(10, 'b')) # >>: 1010 # *
+    print("abc{10:#b}def:", f'abc{10:#b}def') # >>: abc0b1010def # *
+    print("abc{10:b}def:", f'abc{10:b}def') # >>: abc1010def # *
 
 def bool_trial():
-    print("bool(0):", bool(0)) # >> False
-    print("bool(1):", bool(1)) # >> True
-    print("bool(0.0):", bool(0.0)) # >> False
-    print("bool(0.1):", bool(0.1)) # >> True # *
-    print("bool(''):", bool('')) # >> False
-    print("bool('abc'):", bool('abc')) # >> True
-    print("bool([]):", bool([])) # >> False
-    print("bool([1]):", bool([1])) # >> True
-    print("bool({}):", bool({})) # >> False
-    print("bool({'a': 1}):", bool({'a': 1})) # >> True
+    print("bool(0):", bool(0)) # >>: False
+    print("bool(1):", bool(1)) # >>: True
+    print("bool(0.0):", bool(0.0)) # >>: False
+    print("bool(0.1):", bool(0.1)) # >>: True # *
+    print("bool(''):", bool('')) # >>: False
+    print("bool('abc'):", bool('abc')) # >>: True
+    print("bool([]):", bool([])) # >>: False
+    print("bool([1]):", bool([1])) # >>: True
+    print("bool({}):", bool({})) # >>: False
+    print("bool({'a': 1}):", bool({'a': 1})) # >>: True
 
 def callable_trial():
     def func1():
@@ -54,28 +54,28 @@ def callable_trial():
     
     obj1 = MyClass()
 
-    print("callable(func1):", callable(func1)) # >> True
-    print('callable(func1()):', callable(func1())) # >> False
-    print("callable(MyClass):", callable(MyClass)) # >> True
-    print("callable(obj1):", callable(obj1)) # >> False # *
-    print("callable(1):", callable(1)) # >> False
-    print("callable('abc'):", callable('abc')) # >> False
-    print("callable([1,2]):", callable([1,2])) # >> False
+    print("callable(func1):", callable(func1)) # >>: True
+    print('callable(func1()):', callable(func1())) # >>: False
+    print("callable(MyClass):", callable(MyClass)) # >>: True
+    print("callable(obj1):", callable(obj1)) # >>: False # *
+    print("callable(1):", callable(1)) # >>: False
+    print("callable('abc'):", callable('abc')) # >>: False
+    print("callable([1,2]):", callable([1,2])) # >>: False
 
 # transform unicode into character
 def chr_trial():
-    print('chr(65):', chr(65)) # >> A
-    print('chr(97):', chr(97)) # >> a
-    print('chr(8364):', chr(8364)) # >> €
+    print('chr(65):', chr(65)) # >>: A
+    print('chr(97):', chr(97)) # >>: a
+    print('chr(8364):', chr(8364)) # >>: €
 
 def complex_trial():
-    print('complex(1, 2):', complex(1, 2)) # >> (1+2j)
-    print('complex(1.1, 2.2):', complex(1.1, 2.2)) # >> (1.1+2.2j)
-    print('complex("1+2j"):', complex("1+2j")) # >> (1+2j)
-    print('complex("1.1+2.2j"):', complex("1.1+2.2j")) # >> (1.1+2.2j)
+    print('complex(1, 2):', complex(1, 2)) # >>: (1+2j)
+    print('complex(1.1, 2.2):', complex(1.1, 2.2)) # >>: (1.1+2.2j)
+    print('complex("1+2j"):', complex("1+2j")) # >>: (1+2j)
+    print('complex("1.1+2.2j"):', complex("1.1+2.2j")) # >>: (1.1+2.2j)
     # operations
-    print("complex(1, 2) + complex(3, 4):", complex(1, 2) + complex(3, 4)) # >> (4+6j)
-    print("complex(1, 2) * complex(3, 4):", complex(1, 2) * complex(3, 4)) # >> (-5+10j)
+    print("complex(1, 2) + complex(3, 4):", complex(1, 2) + complex(3, 4)) # >>: (4+6j)
+    print("complex(1, 2) * complex(3, 4):", complex(1, 2) * complex(3, 4)) # >>: (-5+10j)
 
 def delattr_trial():
     class MyClass:
