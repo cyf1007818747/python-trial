d3d83335a0fc4a9fbc50cde980be12b2395c6213
Chris Cheng <yifan.cheng.1998@gmail.com>
2024-03-31 18:07:18 +1100

    add leetcode q 739

---
+++ leetcode/stack/lc-739.py
@@ -0,0 +1,25 @@
+
+
+from typing import List
+
+# ! recite !!
+# this is you recite and rewrite from leetcode.cn
+# passed all leetcode tests
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        length = len(temperatures)
+
+        rtn = [0] * length
+
+        stack = []
+
+        for i in range(length):
+            temp = temperatures[i]
+            
+            while stack and temp > temperatures[stack[-1]]:
+                index_top = stack.pop()
+                rtn[index_top] = i - index_top
+
+            stack.append(i)
+
+        return rtn
