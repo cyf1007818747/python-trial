5a5b86a5ddc221e10f0516f2f76675e0c5afd3cd
Chris Cheng <yifan.cheng.1998@gmail.com>
2024-03-31 19:46:06 +1100

    add leetcode 560

---
+++ leetcode/array/lc-560.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if len(nums) == 1:
+            return 1 if nums[0] == k else 0
+        
+        # this is for the case to include all nums from nums[0] # *
+        # ! just recite for now that this is common for prefix sum questions
+        sum_freq = {0: 1}
+
+        summ, count = 0, 0
+
+        for num in nums:
+            summ += num
+            count += sum_freq.get(summ - k, 0)
+            sum_freq[summ] = sum_freq.get(summ, 0) + 1
+
+        return count
